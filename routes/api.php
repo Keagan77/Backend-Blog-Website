69a52e461d7cf3ae21e0124d9d001da232483de5
keagan77 <keagansilveira77@gmail.com>
2025-01-17 12:50:42 +0530

    Image Route added

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 // Route::get('blogs',[BlogController::class,'index']);
 Route::post('blogs',[BlogController::class,'store']);
-// Route::post('save-temp-image',[TempImageController::class,'store']);
+Route::post('save-temp-image',[TempImageController::class,'store']);
 // Route::get('blogs/{id}',[BlogController::class,'show']);
 // Route::put('blogs/{id}',[BlogController::class,'update']);
 // Route::delete('blogs/{id}',[BlogController::class,'destroy']);
