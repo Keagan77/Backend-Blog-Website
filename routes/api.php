d202d97821ff1d6280761d93d936b34966c19223
keagan77 <keagansilveira77@gmail.com>
2025-01-24 16:20:43 +0530

    Update API Added

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::get('blogs',[BlogController::class,'index']);
 Route::post('blogs',[BlogController::class,'store']);
 Route::post('save-temp-image',[TempImageController::class,'store']);
 Route::get('blogs/{id}',[BlogController::class,'show']);
-// Route::put('blogs/{id}',[BlogController::class,'update']);
+Route::put('blogs/{id}',[BlogController::class,'update']);
 // Route::delete('blogs/{id}',[BlogController::class,'destroy']);
 
 
